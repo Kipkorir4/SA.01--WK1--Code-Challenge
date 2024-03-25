41565a87c7da93b1bf42b1677969097557203487
Gideon <keterjr1@gmail.com>
2024-03-25 11:42:40 +0300

    return nhif values

--- Net Salary Calculator.js
+++ Net Salary Calculator.js
@@ -1,21 +1,21 @@
 // Function to calculate NHIF deductions based on gross pay
 function calculateNHIF(grossPay) {
     if (grossPay <= 5999) return 150;
-    else if (grossPay <= 7999) return 300;
-    else if (grossPay <= 11999) return 400;
-    else if (grossPay <= 14999) return 500;
-    else if (grossPay <= 19999) return 600;
-    else if (grossPay <= 24999) return 750;
-    else if (grossPay <= 29999) return 850;
-    else if (grossPay <= 34999) return 900;
-    else if (grossPay <= 39999) return 950;
-    else if (grossPay <= 44999) return 1000;
-    else if (grossPay <= 49999) return 1100;
-    else if (grossPay <= 59999) return 1200;
-    else if (grossPay <= 69999) return 1300;
-    else if (grossPay <= 79999) return 1400;
-    else if (grossPay <= 89999) return 1500;
-    else if (grossPay <= 99999) return 1600;
+    else if (grossPay <= 7999 && grossPay > 5999) return 300;
+    else if (grossPay <= 11999 && grossPay > 7999 ) return 400;
+    else if (grossPay <= 14999 && grossPay > 11999) return 500;
+    else if (grossPay <= 19999 && grossPay > 14999) return 600;
+    else if (grossPay <= 24999 && grossPay > 19999) return 750;
+    else if (grossPay <= 29999 && grossPay > 24999) return 850;
+    else if (grossPay <= 34999 && grossPay > 29999) return 900;
+    else if (grossPay <= 39999 && grossPay > 34999) return 950;
+    else if (grossPay <= 44999 && grossPay > 39999) return 1000;
+    else if (grossPay <= 49999 && grossPay > 44999) return 1100;
+    else if (grossPay <= 59999 && grossPay > 49999) return 1200;
+    else if (grossPay <= 69999 && grossPay > 59999) return 1300;
+    else if (grossPay <= 79999 && grossPay > 69999) return 1400;
+    else if (grossPay <= 89999 && grossPay > 79999) return 1500;
+    else if (grossPay <= 99999 && grossPay > 79999) return 1600;
     else return 1700; // For gross pay of 100,000 and above
 }
 
